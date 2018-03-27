8725e6cb02ec54a17d9d6c9b90d4d36425a4ef17
Ashley Rolleston <ash@block-tree.io>
2018-03-27 12:52:27 +0100

    add network benchmark project with dotnetbenchmark

---
+++ NeoSharp.Network.Benchmark/BM_PeerFactory.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Columns;
+using BenchmarkDotNet.Attributes.Exporters;
+using BenchmarkDotNet.Attributes.Jobs;
+using NeoSharp.Network.DI;
+using NeoSharp.Logging.DI;
+using SimpleInjector;
+using Moq;
+using Microsoft.Extensions.Logging;
+
+namespace NeoSharp.Network.Benchmark
+{       
+    public class BM_PeerFactory
+    {
+        PeerFactory uub;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Container cont = new Container();
+            cont.Register<IPeer, Peer>(Lifestyle.Transient);
+            cont.Register(ConfigureLogger, Lifestyle.Singleton);
+            cont.Register(typeof(ILogger<>), typeof(LoggingAdapter<>));
+            uub = new PeerFactory(cont);
+        }
+
+        private static ILoggerFactory ConfigureLogger()
+        {
+            Mock<ILoggerFactory> mockLoggerFactory = new Mock<ILoggerFactory>();
+            return mockLoggerFactory.Object;
+        }
+
+        [Benchmark]
+        public void createPeers()
+        {
+            for (int i=0; i<10000; i++)
+                uub.Create();
+        }
+
+    }
+}
