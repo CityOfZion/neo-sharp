6434a52762736dd7576c2660935118fce214e558
Paulo Aboim Pinto <aboimpinto@gmail.com>
2018-04-14 01:53:31 +0200

    refactor UtNetworkManager (#39)

--- test/NeoSharp.Core.Test/Network/UTNetworkManager.cs
+++ test/NeoSharp.Core.Test/Network/UTNetworkManager.cs
@@ -1,39 +1,41 @@
-﻿using Microsoft.Extensions.Logging;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using NeoSharp.Core.Network;
+using NeoSharp.TestHelpers;
 
 namespace NeoSharp.Core.Test.Network
 {
     [TestClass]
-    public class UtNetworkManager
+    public class UtNetworkManager : TestBase
     {
-        NetworkManager _uut;
-        Mock<ILogger<NetworkManager>> _mockLogger;
-        Mock<IServer> _mockServer;
-
-        [TestInitialize]
-        public void TestSetup()
-        {
-            _mockLogger = new Mock<ILogger<NetworkManager>>();
-            _mockServer = new Mock<IServer>();
-
-            _uut = new NetworkManager(_mockLogger.Object, _mockServer.Object);
-        }
-
         [TestMethod]
         public void StartNetwork_Starts_Server()
         {
-            _uut.StartNetwork();
-            _mockServer.Verify(m => m.StartServer(), Times.Once);
+            // Arrange 
+            var serverMock = this.AutoMockContainer.GetMock<IServer>();
+
+            var networkManager = this.AutoMockContainer.Create<NetworkManager>();
+
+            // Act
+            networkManager.StartNetwork();
+
+            // Asset
+            serverMock.Verify(x => x.StartServer(), Times.Once);
         }
 
         [TestMethod]
         public void StopNetwork_Stops_Server()
         {
-            _uut.StopNetwork();
-            _mockServer.Verify(m => m.StopServer(), Times.Once);
-        }
+            // Arrange 
+            var serverMock = this.AutoMockContainer.GetMock<IServer>();
 
+            var networkManager = this.AutoMockContainer.Create<NetworkManager>();
+
+            // Act
+            networkManager.StopNetwork();
+
+            // Asset
+            serverMock.Verify(x => x.StopServer(), Times.Once);
+        }
     }
 }
