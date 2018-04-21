0b9137db796a3969287bc56afe44761966a901a8
Alex Guba <6355333+gubanotorious@users.noreply.github.com>
2018-04-21 00:45:14 -0400

    Added Redis Streams API Support to NeoSharp.Persistence.RedisDB (#43)
    * Implemented Repository and Factory Patterns to allow for switching of database providers
    - Defined repository interface
    - Added demo provider implementations for Rocks DB and Cosmos DB
    
    * Initial commit of migrated types and extensions needed to work on persistence repositories
    
    ENUMS
    - /Implementations/Blockchains/LevelDB/DataEntryPrefix.cs -> /Persistence
    
    EXTENSIONS
    - /Helper.cs -> Migrated to NeoSharp.Core/Extensions
    - /IO/Helper.cs -> Migrated to NeoSharp.Core/Extensions
    
    TYPES
    - /IO/ISerializable.cs -> /Types
    - /IO/ICloneable.cs -> /Types
    - /IO/ISerializable.cs -> /Types
    - /IO/Wrappers -> /Types
    - /IO/Json -> /Types
    - /Fixed8.cs -> /Types/Fixed8
    - /BigDecimal.cs -> /Types/BigDecimal
    - /UIntBase.cs -> /Types/UIntBase
    - /UInt160.cs -> /Types/UInt160
    - /UInt256.cs -> /Types/UInt256
    
    NETWORK
    - /Network/IInventory.cs -> /Network/IInventory
    - /Network/InventoryType.cs -> /Network/InventoryType
    
    * Cleanup orphaned database project in filesystem
    
    * - Cleanup and remove NeoSharp.Persistence.Common project that was not needed
    
    * Persistence
    - Removed NeoSharp.Persistence.CosmosDB and NeoSharp.Persistence.RocksDB Persistence Projects since we will do LevelDB and Redis first
    - Added NeoSharp.Persistence.RedisDB Project
    
    VM
    - Added NeoSharp.VM.NeoVM and NeoSharp.VM.HyperVM Projects to handle specific VM implementation / interop / abstraction
    
    * Persistence
    - Removed NeoSharp.Persistence.CosmosDB and NeoSharp.Persistence.RocksDB Persistence Projects since we will do LevelDB and Redis first
    - Added NeoSharp.Persistence.RedisDB Project
    
    VM
    - Added NeoSharp.VM.NeoVM and NeoSharp.VM.HyperVM Projects to handle specific VM implementation / interop / abstraction
    
    * - Moved NeoSharp.VM.HyperVM to src folder
    
    * Cleanup and remove duplicate / misplaced NeoSharp.VM.HyperVM
    
    * Cleanup projects
    
    * Cleanup solution
    
    - Persistence.RedisDB: Added NuGet Package Dependency for StackExchange.Redis
    - Persistence.RedisDB: Added RedisStreamsHelper to wrap Streams API Functionality using StackExchange.Redis

---
+++ src/NeoSharp.Persistence.RedisDB/RedisStreamEntry.cs
@@ -0,0 +1,14 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeoSharp.Persistence.RedisDB
+{
+    public class RedisStreamEntry
+    {
+        public RedisResult Id { get; set; }
+        public RedisResult Key { get; set; }
+        public RedisResult Value { get; set; }
+    }
+}
