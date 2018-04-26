3f3ceb54dce2c9f29f765f0f07a6c8adab8bf2df
Shargon <shargon@gmail.com>
2018-04-26 11:13:34 +0200

    Reorder usings

--- src/NeoSharp.Core/Network/Tcp/TcpPeer.cs
+++ src/NeoSharp.Core/Network/Tcp/TcpPeer.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using NeoSharp.Core.Extensions;
+using System;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
-using NeoSharp.Core.Extensions;
 
 namespace NeoSharp.Core.Network.Tcp
 {
