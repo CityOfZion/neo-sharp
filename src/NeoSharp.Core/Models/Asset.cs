1282d865b573f5086be2735b26a19c5e8695c87c
Alex Guba <6355333+gubanotorious@users.noreply.github.com>
2018-05-05 02:04:58 -0400

    Added first pass of serialization and persistence objects (#58)
    Added first pass on persistence object models with JSON / Binary serialization attributes

---
+++ src/NeoSharp.Core/Models/Asset.cs
@@ -0,0 +1,43 @@
+﻿using NeoSharp.BinarySerialization;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace NeoSharp.Core.Models
+{
+    public class Asset
+    {
+        [BinaryProperty(1)]
+        [JsonProperty("hash")]
+        public string Id;
+
+        [BinaryProperty(2)]
+        [JsonProperty("type")]
+        public AssetType AssetType;
+
+#if !DEBUG //TODO: Chinese characters are having an issue in the VS Debugger?  We can't serialize Chinese Chars?
+        [BinaryProperty(3)]
+        [JsonProperty("name")]
+        public string Name;
+#endif
+
+        [BinaryProperty(4)]
+        [JsonProperty("amount")]
+        public double Amount;
+
+        [BinaryProperty(5)]
+        [JsonProperty("precision")]
+        public byte Precision;
+
+        [BinaryProperty(6)]
+        [JsonProperty("owner")]
+        public string Owner;
+
+        [BinaryProperty(7)]
+        [JsonProperty("admin")]
+        public string Admin;
+
+        [BinaryProperty(8)]
+        [JsonProperty("transactions")]
+        public Transaction[] Transactions = new Transaction[0];
+    }
+}
---
+++ src/NeoSharp.Core/Models/TransactionAttributeUsage.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeoSharp.Core.Models
+{
+    [Serializable]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TransactionAttributeUsage : byte
+    {
+        /// <summary>
+        /// Hash value of external contract
+        /// </summary>
+        ContractHash = 0x00,
+        /// <summary>
+        /// The public key used for the exchange of the ECDH keys.The first byte of the public key is 0x02
+        /// </summary>
+        ECDH02 = 0x02,
+        /// <summary>
+        /// Public key used for ECDH key exchange. The first byte of the public key is 0x03
+        /// </summary>
+        ECDH03 = 0x03,
+        /// <summary>
+        /// Used for additional verification of transactions
+        /// </summary>
+        Script = 0x20,
+
+        Vote = 0x30,
+
+        DescriptionUrl = 0x81,
+        Description = 0x90,
+
+        Hash1 = 0xa1,
+        Hash2 = 0xa2,
+        Hash3 = 0xa3,
+        Hash4 = 0xa4,
+        Hash5 = 0xa5,
+        Hash6 = 0xa6,
+        Hash7 = 0xa7,
+        Hash8 = 0xa8,
+        Hash9 = 0xa9,
+        Hash10 = 0xaa,
+        Hash11 = 0xab,
+        Hash12 = 0xac,
+        Hash13 = 0xad,
+        Hash14 = 0xae,
+        Hash15 = 0xaf,
+
+        /// <summary>
+        /// Note
+        /// </summary>
+        Remark = 0xf0,
+        Remark1 = 0xf1,
+        Remark2 = 0xf2,
+        Remark3 = 0xf3,
+        Remark4 = 0xf4,
+        Remark5 = 0xf5,
+        Remark6 = 0xf6,
+        Remark7 = 0xf7,
+        Remark8 = 0xf8,
+        Remark9 = 0xf9,
+        Remark10 = 0xfa,
+        Remark11 = 0xfb,
+        Remark12 = 0xfc,
+        Remark13 = 0xfd,
+        Remark14 = 0xfe,
+        Remark15 = 0xff
+    }
+}
