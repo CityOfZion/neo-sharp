6d5adeb5de69b5975075939b06701580da27c137
Shargon <shargon@gmail.com>
2018-05-16 17:44:18 +0200

    Easy style definition

--- src/NeoSharp.Application/Client/ConsoleOutputStyle.cs
+++ src/NeoSharp.Application/Client/ConsoleOutputStyle.cs
@@ -1,34 +1,44 @@
-﻿namespace NeoSharp.Application.Client
+﻿using NeoSharp.Application.Attributes;
+using System;
+
+namespace NeoSharp.Application.Client
 {
     public enum ConsoleOutputStyle : byte
     {
         /// <summary>
         /// Regular output
         /// </summary>
+        [ConsoleOutputStyle(Foreground = ConsoleColor.White)]
         Output,
         /// <summary>
         /// Prompt
         /// </summary>
+        [ConsoleOutputStyle(Foreground = ConsoleColor.DarkGreen)]
         Prompt,
         /// <summary>
         /// Input
         /// </summary>
+        [ConsoleOutputStyle(Foreground = ConsoleColor.Green)]
         Input,
         /// <summary>
         /// Information
         /// </summary>
+        [ConsoleOutputStyle(Foreground = ConsoleColor.Yellow)]
         Information,
         /// <summary>
         /// Autocomplete
         /// </summary>
+        [ConsoleOutputStyle(Foreground = ConsoleColor.DarkYellow)]
         Autocomplete,
         /// <summary>
         /// Autocomplete match
         /// </summary>
+        [ConsoleOutputStyle(Foreground = ConsoleColor.Yellow)]
         AutocompleteMatch,
         /// <summary>
         /// Error
         /// </summary>
+        [ConsoleOutputStyle(Foreground = ConsoleColor.Red)]
         Error
     }
 }
