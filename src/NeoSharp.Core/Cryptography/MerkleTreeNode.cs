83028f4a54b2178f218036044b80b00ed732c163
belane <belane@users.noreply.github.com>
2018-05-18 21:30:08 +0200

    Merkle tree optimization and Unit Tests (#94)
    * Removing unsafe from int e uint conversion (#26)
    
    * Revert "Removing unsafe from int e uint conversion (#26)" (#27)
    
    This reverts commit 018fe9fd95039b6fcaf26fc0e0ae64be91aa115e.
    
    * ACL Unit Tests and some improvement
    
    * Base58 UT and coments
    
    * Update UtBase58.cs
    
    * Update UtBase58.cs
    
    * Merkle tree optimization and Unit Tests
    
    * Update UtMerkleTree.cs

--- src/NeoSharp.Core/Cryptography/MerkleTreeNode.cs
+++ src/NeoSharp.Core/Cryptography/MerkleTreeNode.cs
@@ -2,15 +2,46 @@
 
 namespace NeoSharp.Core.Cryptography
 {
-    internal class MerkleTreeNode
+    public class MerkleTreeNode
     {
+        /// <summary>
+        /// Node hash
+        /// </summary>
         public UInt256 Hash;
+        /// <summary>
+        /// Parent node
+        /// </summary>
         public MerkleTreeNode Parent;
+        /// <summary>
+        /// Left child node
+        /// </summary>
         public MerkleTreeNode LeftChild;
+        /// <summary>
+        /// Right child node
+        /// </summary>
         public MerkleTreeNode RightChild;
-
+        /// <summary>
+        /// Is root
+        /// </summary>
+        public bool IsRoot => Parent == null;
+        /// <summary>
+        /// Is leaf
+        /// </summary>
         public bool IsLeaf => LeftChild == null && RightChild == null;
 
-        public bool IsRoot => Parent == null;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MerkleTreeNode()
+        {
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="hash">Hash</param>
+        public MerkleTreeNode(UInt256 hash)
+        {
+            Hash = hash;
+        }
     }
 }
