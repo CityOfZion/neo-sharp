09ad72fb6445bbdf8e325830dc45985a5f7bcd8a
Shargon <shargon@gmail.com>
2018-06-16 09:45:46 +0200

    Remove unused methods

--- src/NeoSharp.BinarySerialization/Extensions/BinaryReaderExtensions.cs
+++ src/NeoSharp.BinarySerialization/Extensions/BinaryReaderExtensions.cs
@@ -1,36 +1,9 @@
-﻿using System.Linq;
-using System.Text;
+﻿using System.Text;
 
 namespace System.IO
 {
     public static class BinaryReaderExtensions
     {
-        public static byte[] ReadBytesWithGrouping(this BinaryReader reader)
-        {
-            const int groupSize = 16;
-            using (var ms = new MemoryStream())
-            {
-                int padding;
-                do
-                {
-                    var group = reader.ReadBytes(groupSize);
-                    padding = reader.ReadByte();
-                    if (padding > groupSize)
-                        throw new FormatException();
-                    var count = groupSize - padding;
-                    if (count > 0)
-                        ms.Write(group, 0, count);
-                } while (padding == 0);
-                return ms.ToArray();
-            }
-        }
-
-        public static string ReadFixedString(this BinaryReader reader, int length)
-        {
-            var data = reader.ReadBytes(length);
-            return Encoding.UTF8.GetString(data.TakeWhile(p => p != 0).ToArray());
-        }
-
         public static byte[] ReadVarBytes(this BinaryReader reader, int max = 0X7fffffc7)
         {
             return reader.ReadBytes((int)reader.ReadVarInt((ulong)max));
--- src/NeoSharp.BinarySerialization/Extensions/BinaryWriterExtensions.cs
+++ src/NeoSharp.BinarySerialization/Extensions/BinaryWriterExtensions.cs
@@ -4,40 +4,6 @@ namespace System.IO
 {
     public static class BinaryWriterExtensions
     {
-        public static void WriteBytesWithGrouping(this BinaryWriter writer, byte[] value)
-        {
-            const int groupSize = 16;
-            var index = 0;
-            var remain = value.Length;
-            while (remain >= groupSize)
-            {
-                writer.Write(value, index, groupSize);
-                writer.Write((byte)0);
-                index += groupSize;
-                remain -= groupSize;
-            }
-            if (remain > 0)
-                writer.Write(value, index, remain);
-            var padding = groupSize - remain;
-            for (var i = 0; i < padding; i++)
-                writer.Write((byte)0);
-            writer.Write((byte)padding);
-        }
-
-        public static void WriteFixedString(this BinaryWriter writer, string value, int length)
-        {
-            if (value == null)
-                throw new ArgumentNullException(nameof(value));
-            if (value.Length > length)
-                throw new ArgumentException();
-            var bytes = Encoding.UTF8.GetBytes(value);
-            if (bytes.Length > length)
-                throw new ArgumentException();
-            writer.Write(bytes);
-            if (bytes.Length < length)
-                writer.Write(new byte[length - bytes.Length]);
-        }
-
         public static int WriteVarBytes(this BinaryWriter writer, byte[] value)
         {
             int ret = writer.WriteVarInt(value.Length);
