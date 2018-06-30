cecaf965622cbf3dcda8af5c6f3c1cc4b169c66a
Alex Guba <6355333+gubanotorious@users.noreply.github.com>
2018-06-30 03:00:59 -0400

    Added sample unit test setup for redis (#156)
    * Added sample unit test setup for redis

---
+++ test/NeoSharp.Persistence.Redis.Tests/RepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeoSharp.Core.Persistence;
+using NeoSharp.Persistence.RedisDB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeoSharp.Persistence.Redis.Tests
+{
+    //[TestClass]
+    //public class RepositoryTest
+    //{
+    //    RedisDbRepository _repo;
+
+    //    [TestInitialize]
+    //    public void TestSetup()
+    //    {
+    //        RedisDbConfiguration config = new RedisDbConfiguration
+    //        {
+    //            ConnectionString = "localhost",
+    //            DatabaseId = 0,
+    //            StorageFormat = RepositoryPersistenceFormat.JSON
+    //        };
+
+    //        _repo = new RedisDbRepository(config, new BinarySerialization.BinarySerializer(), new BinarySerialization.BinaryDeserializer());
+    //    }
+    //}
+}
