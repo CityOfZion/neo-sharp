0b9f642bcfe7da229b1f0de3eb7b2f4d3a40517c
Shargon <shargon@gmail.com>
2018-07-01 14:40:28 +0200

    Appsettings mainnet (#160)
    * Genesis
    
    * Add genesis to blockchain
    
    * appsettings.mainnet
    
    * testnet
    
    * fix

--- src/NeoSharp.Core/Network/NetworkManager.cs
+++ src/NeoSharp.Core/Network/NetworkManager.cs
@@ -1,23 +1,37 @@
-﻿
-using NeoSharp.Core.Logging;
+﻿using NeoSharp.Core.Logging;
 
 namespace NeoSharp.Core.Network
 {
     public class NetworkManager : INetworkManager
     {
+        #region Private fields
+
         private readonly IServer _server;
 
+        #endregion
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="logger">Logger</param>
+        /// <param name="serverInit">Server</param>
         public NetworkManager(ILogger<NetworkManager> logger, IServer serverInit)
         {
             _server = serverInit;
             logger.LogInformation("Network Manager Initialised");
         }
 
+        /// <summary>
+        /// Start network
+        /// </summary>
         public void StartNetwork()
         {
             _server.Start();
         }
 
+        /// <summary>
+        /// Stop network
+        /// </summary>
         public void StopNetwork()
         {
             _server.Stop();
