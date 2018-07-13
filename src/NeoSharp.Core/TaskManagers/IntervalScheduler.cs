8be53831f2cbe12d68f33eed6b4a6d1a7f1fa089
Gil Lopes Bueno <melanke.hc@gmail.com>
2018-07-13 10:13:25 +0800

    Refactoring and RunTask (#215)

---
+++ src/NeoSharp.Core/TaskManagers/IntervalScheduler.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NeoSharp.Core.TaskManagers
+{
+    public abstract class IntervalScheduler
+    {
+        private static readonly List<CancellationTokenSource> _cancellationTokens = new List<CancellationTokenSource>();
+
+        public static async Task RunTask(TimeSpan interval, CancellationTokenSource cancellationToken, Func<Task> action)
+        {
+            CheckAndStoreCancellationToken(cancellationToken);
+            
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await action();
+                await Task.Delay(interval, cancellationToken.Token);
+            }
+        }
+
+        public static async Task RunTask(TimeSpan interval, CancellationTokenSource cancellationToken, int repetitions, Func<Task> action)
+        {
+            CheckAndStoreCancellationToken(cancellationToken);
+            
+            var countRepeat = 0;
+
+            while (!cancellationToken.IsCancellationRequested && countRepeat < repetitions)
+            {
+                await action();
+                await Task.Delay(interval, cancellationToken.Token);
+                countRepeat++;
+            }
+        }
+
+        public static async Task RunTask(TimeSpan interval, int repetitions, Func<Task> action)
+        {
+            await RunTask(interval, new CancellationTokenSource(), repetitions, action);
+        }
+
+        public static async void Run(TimeSpan interval, CancellationTokenSource cancellationToken, Func<Task> action)
+        {
+            await RunTask(interval, cancellationToken, action);
+        }
+
+        public static async void Run(TimeSpan interval, CancellationTokenSource cancellationToken, int repetitions,
+            Func<Task> action)
+        {
+            await RunTask(interval, cancellationToken, repetitions, action);
+        }
+
+        public static async void Run(TimeSpan interval, int repetitions, Func<Task> action)
+        {
+            await RunTask(interval, new CancellationTokenSource(), repetitions, action);
+        }
+
+        public static void CancelAllTasks()
+        {
+            _cancellationTokens.ForEach(c => c.Cancel());
+        }
+
+        private static void CheckAndStoreCancellationToken(CancellationTokenSource cancellationToken)
+        {
+            if (cancellationToken == null)
+            {
+                throw new ArgumentNullException(nameof(cancellationToken));
+            }
+            
+            if (!_cancellationTokens.Contains(cancellationToken))
+            {
+                _cancellationTokens.Add(cancellationToken);
+            }
+        } 
+    }
+}
