d9976aa09b6a5ff3f3d529ef48e1215975bf7f4e
Paulo Aboim Pinto <aboimpinto@gmail.com>
2018-07-17 16:14:33 +0200

    Move NativeCrypto to test project (#236)
    * RocksDbRepository reach 42% cover
    
    * Move NativeCrypto class to test project because it's only used there.
    
    * Update RocksDbRepository.cs

--- test/NeoSharp.Core.Test/Cryptography/NativeCrypto.cs
+++ test/NeoSharp.Core.Test/Cryptography/NativeCrypto.cs
@@ -1,17 +1,12 @@
 ﻿using System;
 using System.Security.Cryptography;
+using NeoSharp.Core.Cryptography;
 
-namespace NeoSharp.Core.Cryptography
+namespace NeoSharp.Core.Test.Cryptography
 {
     public class NativeCrypto : BouncyCastleCrypto
     {
-        /// <summary>
-        /// Sha256 digests
-        /// </summary>
-        /// <param name="message">Message bytearray</param>
-        /// <param name="offset">Offset</param>
-        /// <param name="count">Length</param>
-        /// <returns>Hash bytearray</returns>
+        /// <inheritdoc />
         public override byte[] Sha256(byte[] message, int offset, int count)
         {
             byte[] hash;
@@ -22,15 +17,10 @@ namespace NeoSharp.Core.Cryptography
             return hash;
         }
 
-        /// <summary>
-        /// Encrypt using ECB
-        /// </summary>
-        /// <param name="data">Data</param>
-        /// <param name="key">Key</param>
-        /// <returns>Bytearray</returns>
+        /// <inheritdoc />
         public override byte[] AesEncrypt(byte[] data, byte[] key)
         {
-            using (Aes aes = Aes.Create())
+            using (var aes = Aes.Create())
             {
                 aes.Key = key;
                 aes.Mode = CipherMode.ECB;
@@ -43,67 +33,50 @@ namespace NeoSharp.Core.Cryptography
             }
         }
 
-        /// <summary>
-        /// Decrypt using ECB
-        /// </summary>
-        /// <param name="data">Data</param>
-        /// <param name="key">Key</param>
-        /// <returns>Bytearray</returns>
+        /// <inheritdoc />
         public override byte[] AesDecrypt(byte[] data, byte[] key)
         {
-            using (Aes aes = Aes.Create())
+            using (var aes = Aes.Create())
             {
                 aes.Key = key;
                 aes.Mode = CipherMode.ECB;
                 aes.Padding = PaddingMode.None;
 
-                using (ICryptoTransform decryptor = aes.CreateDecryptor())
+                using (var decryptor = aes.CreateDecryptor())
                 {
                     return decryptor.TransformFinalBlock(data, 0, data.Length);
                 }
             }
         }
 
-        /// <summary>
-        /// Encrypt using CBC
-        /// </summary>
-        /// <param name="data">Data</param>
-        /// <param name="key">Key</param>
-        /// <param name="iv">IV</param>
-        /// <returns>Bytearray</returns>
+        /// <inheritdoc />
         public override byte[] AesEncrypt(byte[] data, byte[] key, byte[] iv)
         {
             if (data == null || key == null || iv == null) throw new ArgumentNullException();
             if (data.Length % 16 != 0 || key.Length != 32 || iv.Length != 16) throw new ArgumentException();
 
-            using (Aes aes = Aes.Create())
+            using (var aes = Aes.Create())
             {
                 aes.Padding = PaddingMode.None;
 
-                using (ICryptoTransform encryptor = aes.CreateEncryptor(key, iv))
+                using (var encryptor = aes.CreateEncryptor(key, iv))
                 {
                     return encryptor.TransformFinalBlock(data, 0, data.Length);
                 }
             }
         }
 
-        /// <summary>
-        /// Decrypt using CBC
-        /// </summary>
-        /// <param name="data">Data</param>
-        /// <param name="key">Key</param>
-        /// <param name="iv">IV</param>
-        /// <returns>Bytearray</returns>
+        /// <inheritdoc />
         public override byte[] AesDecrypt(byte[] data, byte[] key, byte[] iv)
         {
             if (data == null || key == null || iv == null) throw new ArgumentNullException();
             if (data.Length % 16 != 0 || key.Length != 32 || iv.Length != 16) throw new ArgumentException();
 
-            using (Aes aes = Aes.Create())
+            using (var aes = Aes.Create())
             {
                 aes.Padding = PaddingMode.None;
 
-                using (ICryptoTransform decryptor = aes.CreateDecryptor(key, iv))
+                using (var decryptor = aes.CreateDecryptor(key, iv))
                 {
                     return decryptor.TransformFinalBlock(data, 0, data.Length);
                 }
