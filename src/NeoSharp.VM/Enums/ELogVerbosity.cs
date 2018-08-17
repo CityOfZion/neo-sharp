34c676aad4bce133f64b8234180fd4014ee76377
Shargon <shargon@gmail.com>
2018-08-17 10:56:46 +0200

    Remove stack logs (#321)
    * Remove stack logs
    
    * Fix

--- src/NeoSharp.VM/Enums/ELogVerbosity.cs
+++ src/NeoSharp.VM/Enums/ELogVerbosity.cs
@@ -13,32 +13,6 @@ namespace NeoSharp.VM
         /// <summary>
         /// Enable step into logs
         /// </summary>
-        StepInto = 0x01,
-
-        /// <summary>
-        /// ExecutionContextStack changes
-        /// </summary>
-        ExecutionContextStackChanges = 0x02,
-        /// <summary>
-        /// EvaluationStack changes
-        /// </summary>
-        EvaluationStackChanges = 0x04,
-        /// <summary>
-        /// AltStack changes
-        /// </summary>
-        AltStackChanges = 0x08,
-        /// <summary>
-        /// ResultStack changes
-        /// </summary>
-        ResultStackChanges = 0x10,
-
-        /// <summary>
-        /// All
-        /// </summary>
-        All = StepInto | ExecutionContextStackChanges | EvaluationStackChanges | AltStackChanges| ResultStackChanges,
-        /// <summary>
-        /// Stack changes
-        /// </summary>
-        StackChanges = ExecutionContextStackChanges | EvaluationStackChanges | AltStackChanges | ResultStackChanges,
+        StepInto = 0x01
     }
 }
--- src/NeoSharp.VM/Types/ExecutionEngineLogger.cs
+++ src/NeoSharp.VM/Types/ExecutionEngineLogger.cs
@@ -17,22 +17,6 @@
         /// On StepInto
         /// </summary>
         public event delOnStepInto OnStepInto;
-        /// <summary>
-        /// On ExecutionContextChange
-        /// </summary>
-        public event delOnExecutionContextStackChange OnExecutionContextChange;
-        /// <summary>
-        /// On AltStackChange
-        /// </summary>
-        public event delOnStackItemsStackChange OnAltStackChange;
-        /// <summary>
-        /// On EvaluationStackChange
-        /// </summary>
-        public event delOnStackItemsStackChange OnEvaluationStackChange;
-        /// <summary>
-        /// On ResultStackChange
-        /// </summary>
-        public event delOnStackItemsStackChange OnResultStackChange;
 
         #endregion
 
@@ -58,50 +42,6 @@
         public virtual void RaiseOnStepInto(IExecutionContext context)
         {
             OnStepInto?.Invoke(context);
-        }
-        /// <summary>
-        /// Raise OnExecutionContextChange
-        /// </summary>
-        /// <param name="stack">Stack</param>
-        /// <param name="item">Item</param>
-        /// <param name="index">Index</param>
-        /// <param name="operation">Operation</param>
-        public virtual void RaiseOnExecutionContextChange(IStack<IExecutionContext> stack, IExecutionContext item, int index, ELogStackOperation operation)
-        {
-            OnExecutionContextChange?.Invoke(stack, item, index, operation);
-        }
-        /// <summary>
-        /// Raise OnResultStackChange
-        /// </summary>
-        /// <param name="stack">Stack</param>
-        /// <param name="item">Item</param>
-        /// <param name="index">Index</param>
-        /// <param name="operation">Operation</param>
-        public virtual void RaiseOnResultStackChange(IStackItemsStack stack, IStackItem item, int index, ELogStackOperation operation)
-        {
-            OnResultStackChange?.Invoke(stack, item, index, operation);
-        }
-        /// <summary>
-        /// Raise OnAltStackChange
-        /// </summary>
-        /// <param name="stack">Stack</param>
-        /// <param name="item">Item</param>
-        /// <param name="index">Index</param>
-        /// <param name="operation">Operation</param>
-        public virtual void RaiseOnAltStackChange(IStackItemsStack stack, IStackItem item, int index, ELogStackOperation operation)
-        {
-            OnAltStackChange?.Invoke(stack, item, index, operation);
-        }
-        /// <summary>
-        /// Raise OnEvaluationStackChange
-        /// </summary>
-        /// <param name="stack">Stack</param>
-        /// <param name="item">Item</param>
-        /// <param name="index">Index</param>
-        /// <param name="operation">Operation</param>
-        public virtual void RaiseOnEvaluationStackChange(IStackItemsStack stack, IStackItem item, int index, ELogStackOperation operation)
-        {
-            OnEvaluationStackChange?.Invoke(stack, item, index, operation);
         }
 
         #endregion
