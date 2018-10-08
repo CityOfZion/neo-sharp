8ff97ac7a2c74d2166de9eb75e5e8d9f6d191c47
belane <belane@users.noreply.github.com>
2018-10-08 19:45:36 +0200

    Remove NeoSharp.VM.Interop (#495)
    clean up

--- src/NeoSharp.VM.Interop/Native/MacCore.cs
+++
@@ -1,9 +0,0 @@
-﻿using NeoSharp.VM.Interop.Enums;
-
-namespace NeoSharp.VM.Interop.Native
-{
-    internal class MacCore : LinuxCore
-    {
-        public MacCore() : base(EPlatform.Mac, ".so") { }
-    }
-}
